28f13874c3f100092f5a4b539c08dba86d2221e4
SY Young <r38411@yahoo.com>
2018-06-03 17:54:58 +0800

    assignment 1

--- Week1/ClassNote.java
+++ Week1/ClassNote.java
@@ -9,5 +9,35 @@ public class ClassNote {
 			//	Print(tree.key)
 			//	InOrderTraversal(tree.right)
 		}
+		
+		public void PreOrderTraversal(Tree tree) {
+			//	if tree = nil:
+			//		return
+			//	Print(tree.key)
+			//	PreOrderTraversal(tree.left)
+			//	PreOrderTraversal(tree.right)
+		}
+		
+		public void PostOrderTraversal(Tree tree) {
+			//	if tree = nil:
+			//		return
+			//	PostOrderTraversal(tree.left)
+			//	PostOrderTraversal(tree.right)
+			//	Print(tree.key)
+		}
+		
+		public void LevelTraversal(Tree tree) {
+			// this is breadth-first search
+			//	if tree = nil: return
+			//	Queue q
+			//	q.Enqueue(tree)
+			//	while not q.Empty():
+			//		node <- q.Dequeue()
+			//		Print(node)
+			//		if node.left != nil:
+			//			q.Enqueue(node.left)
+			//		if node.right != nil
+			//			q.Enqueue(node.right)
+		}
 	}
 }

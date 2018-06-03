68ef36ebd9a1d9cd9ad5184d7454d66e5490bc30
SY Young <r38411@yahoo.com>
2018-06-03 17:02:03 +0800

    init the week1

---
+++ Week1/ClassNote.java
@@ -0,0 +1,13 @@
+package Week1;
+
+public class ClassNote {
+	public class Tree {
+		public void InOrderTraversal(Tree tree) {
+			// if tree = nil
+			//	return
+			//	InOrderTraversal(tree.left)
+			//	Print(tree.key)
+			//	InOrderTraversal(tree.right)
+		}
+	}
+}
